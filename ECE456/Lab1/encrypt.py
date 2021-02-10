82813c143eb7fdb883de40641d235a45c8b78d3e
KevinAlamo <31712869+KevinAlamo@users.noreply.github.com>
2021-02-10 00:26:28 -0700

    working on encrypt

--- ECE456/Lab1/encrypt.py
+++ ECE456/Lab1/encrypt.py
@@ -6,3 +6,31 @@ def readKeys(filename, perm):
 
     keyFile.close()
     return keys
+
+def readData(filename, perm):
+    dataFile = open(filename,perm)
+    data = []
+    while(True):
+        d = dataFile.read(2)
+        if d == b'':
+            break
+    data.append(d)
+    dataFile.close()
+    return data
+
+
+def desEncrypt(data, key):
+    l = data[:1]
+    r = data[1:]
+    out_r = int.from_bytes(1, byteorder="big") ^ int.from_bytes(key, byteorder="big")
+    out_l = r
+    return out_l + out_r
+
+
+def encrypt(input_data, keys):
+    encrypted = []
+    for data in input_data:
+        for key in keys:
+            data = desEncrypt(data, key)
+        encrypted.append(data)
+    return encrypted

8e2806244e30fe6ae72291ecc2264271ac750725
KevinAlamo <31712869+KevinAlamo@users.noreply.github.com>
2021-02-12 11:20:45 -0700

    working on sender

--- ECE456/Lab2/sender.py
+++ ECE456/Lab2/sender.py
@@ -1,15 +1,5 @@
 import sys
-
-def readData(fname, perm):
-    dataFile = open(fname, perm)
-    data = []
-    while(True):
-        d = dataFile.read(2)
-        if d == b'':
-            break
-        data.append(d)
-    dataFile.close()
-    return data
+from ECE456.Lab1 import encrypt
 
 
 def processIP(ip_addr):
@@ -25,15 +15,21 @@ def processIP(ip_addr):
 
     res.append(ip_addr[dot+1:])
 
-    return res
+    rtn = ''
+    for i in range(0,4):
+        res[i] = format(int(res[i]), "08b")
+        rtn = rtn + res[i]
+    return rtn
 
 
 def calcUdpLen():
-    return len(info)*2 + 8  # length of data in bytes and the rest of the header
+    return len(info) * 2 + 8  # length of data in bytes and the rest of the header
 
 
-def checksum():
-    pass
+def checksum():  # = psuedoh + sport + des_port + udpL + data + checksum(done at end)
+    # psuedoh = sIP + dIP + zeros + protocol + udpL
+    res =
+
 
 
 if __name__ == '__main__':
@@ -55,9 +51,16 @@ if __name__ == '__main__':
     de_port = int(de_port)
     de_port = format(de_port, "016b")
 
-    info = readData(filename, 'rb')
+    info = encrypt.readData(filename, 'rb')
     udpL = calcUdpLen()  # number of bytes
     udpL = format(udpL, "016b")  # changing to 16 bit representation
+
+    zeros = format(0, "08b")
+    protocol = format(17, "08b")
+    # psuedoh = sIP + dIP + zeros + protocol + udpL
+
+    # encrypt.encrypt(info, )
+    # print(int(protocol, 2))
 
     checksum()
 

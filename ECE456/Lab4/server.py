8ece0cf2684d6daf41cbb080493dd6e84a622011
KevinAlamo <31712869+KevinAlamo@users.noreply.github.com>
2021-03-09 21:47:22 -0700

    working on Lab4

--- ECE456/Lab4/server.py
+++ ECE456/Lab4/server.py
@@ -81,7 +81,7 @@ if __name__ == '__main__':
             data = sendMsg(msgs)
             print(data)
             i = i + 1
-            server_socket.sendto(data.encode(), ())  # send data to the client
+            server_socket.sendto(data.encode(), (address, port))  # send data to the client
 
     # conn.close()  # close the connection
 

d3a8a387a2e7e7ae4a95119aa6a3eabce9be1759
KevinAlamo <kevalamo@rams.colostate.edu>
2021-03-13 21:13:59 -0700

    updating lab4 for tar

---
+++ ECE456/Lab4/encrypt.py
@@ -0,0 +1,68 @@
+import sys
+
+# reads in the keys
+def readKeys(filename, perm):
+    keyFile = open(filename, perm)
+    keys = []
+    for i in range(8):
+        keys.append(keyFile.read(1))
+
+    keyFile.close()
+    return keys
+
+# reads in the data
+def readData(filename, perm):
+    dataFile = open(filename, perm)
+    data = []
+    while(True):
+        d = dataFile.read(2)
+        if d == b'':
+            break
+        data.append(d)
+    dataFile.close()
+    return data
+
+
+# DES encryption method
+def desEncrypt(dat, key):
+    l = dat[:1]
+    r = dat[1:]
+    out_r = int.from_bytes(l, byteorder="big") ^ int.from_bytes(key, byteorder="big")
+    out_r = out_r.to_bytes(1, byteorder="big")
+    out_l = r
+    return out_l + out_r
+
+
+# encrypts the data
+def encrypt(input_data, keys):
+    encrypted = []
+    odd = 0
+    for text in input_data:
+        if len(text) == 1:  # case for padding
+            text = text + b' '
+            odd = 1
+        for key in keys:
+            text = desEncrypt(text, key)
+        if odd == 1:  # removes the padding
+            encrypted.append(text[:1])
+        else:
+            encrypted.append(text)
+    return encrypted
+
+
+# writes to output file
+def outputFile(encrypted_text):
+    file = open('encrypted_text.txt', 'wb')
+    for c in encrypted_text:
+        file.write(c)
+    file.close()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        raise ValueError('No key or data file')
+
+    keys = readKeys(sys.argv[1], 'rb')
+    data = readData(sys.argv[2], 'rb')
+    encrypted = encrypt(data, keys)
+    outputFile(encrypted)
---
+++ ECE456/Lab4/sender.py
@@ -0,0 +1,132 @@
+import math
+import sys
+import encrypt
+
+
+# processes given ip
+def processIP(ip_addr):
+    res = []
+    dot = ip_addr.find(".")
+    res.append(ip_addr[0:dot])
+
+    dot2 = ip_addr.find(".", dot + 1)
+    res.append(ip_addr[dot+1:dot2])
+
+    dot = ip_addr.find(".", dot2 + 1)
+    res.append(ip_addr[dot2+1:dot])
+
+    res.append(ip_addr[dot+1:])
+
+    rtn = ''
+    for i in range(0,4):
+        res[i] = format(int(res[i]), "08b")
+        rtn = rtn + res[i]
+    return rtn
+
+
+# calculates the total length
+def calcUdpLen():
+    res = 0
+    for x in info:
+        res = res + len(x)
+    return res + 8  # length of data in bytes and the rest of the header
+
+
+# calculate the checksum
+def checksum():
+    res = int(sIP[:16], 2) + int(sIP[16:], 2) + int(dIP[:16], 2) + int(dIP[16:], 2) + 0 + 17 + udpL
+    res = res + so_port + de_port + udpL
+    for d in info:
+        if len(d) == 1:
+            d = d + b'\x00'
+        res = res + int.from_bytes(d, "big")
+
+    mask = 0b1111111111111111
+    low16 = res & mask
+    remainder = res >> 16
+    while remainder > 0:
+        res = low16 + remainder
+        low16 = res & mask
+        remainder = res >> 16
+
+    # res = res + res
+    # low16 = res & mask
+    # remainder = res >> 16
+    # while remainder > 0:
+    #     res = low16 + remainder
+    #     low16 = res & mask
+    #     remainder = res >> 16
+
+    # calculating number of bits
+    # in the number
+    x = int(math.log2(res)) + 1
+
+    # Inverting the bits one by one
+    for i in range(x):
+        res = (res ^ (1 << i))
+
+    return res
+
+
+# sets up the datagram that will be sent
+def setDatagram():
+    datagram = []
+    datagram.append(so_port.to_bytes(2, 'big'))
+    datagram.append(de_port.to_bytes(2, 'big'))
+    datagram.append(udpL.to_bytes(2, 'big'))
+    datagram.append(check.to_bytes(2, 'big'))
+
+    for x in info:
+        datagram.append(x)
+    return datagram
+
+
+def addpadding(info):
+    dataLen = 0
+    for i in info:
+        dataLen = dataLen + len(i)
+
+    if dataLen%2 == 1:
+        info[len(info)-1] = info[len(info)-1] + b'\x00'
+
+
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 7:
+        raise ValueError('Incorrect num of args')
+    # grabbing the arguments
+    filename = sys.argv[1]
+    sourceIP = sys.argv[2]
+    destIP = sys.argv[3]
+    so_port = sys.argv[4]
+    de_port = sys.argv[5]
+    datagram_output = sys.argv[6]
+    # processing the inputs
+    sIP = processIP(sourceIP)
+    dIP = processIP(destIP)
+
+    so_port = int(so_port)
+    de_port = int(de_port)
+
+    info = encrypt.readData(filename, 'rb')
+    udpL = calcUdpLen()  # number of bytes
+    print("LENGTH: " + str(udpL))
+
+    # psuedoh = sIP + dIP + zeros + protocol + udpL
+
+    # encrypt.encrypt(info, )
+    # print(int(protocol, 2))
+
+
+    keys = encrypt.readKeys("../Lab4/keyall1", 'rb')
+    info = encrypt.encrypt(info, keys)
+    addpadding(info)
+    check = checksum()
+    print("THE CHECK: " + str(check))
+    datag = setDatagram()
+
+    file = open(datagram_output, 'wb')  # writing to the file
+    for c in datag:
+        file.write(c)
+    file.close()

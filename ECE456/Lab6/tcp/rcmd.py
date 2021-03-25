4334fbf228d73a99206d3d693d3b902a06897eb7
KevinAlamo <kevalamo@rams.colostate.edu>
2021-03-24 22:38:26 -0600

    working on lab 6

--- ECE456/Lab6/tcp/rcmd.py
+++ ECE456/Lab6/tcp/rcmd.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
         raise ValueError('Incorrect num of args')
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create a socket object
     hostN = sys.argv[1]
-    port = sys.argv[2]  # Reserve a port for your service.
+    port = int(sys.argv[2])  # Reserve a port for your service.
     host = socket.gethostbyname(hostN)
 
     s.connect((host, port))
@@ -16,11 +16,13 @@ if __name__ == '__main__':
     cmd = sys.argv[5]
     cmdToSend = execTime + "@" + delay + "@" + cmd + "@"
 
-    s.send(bytes(cmd.encode("ascii")))  # send the file name
+    s.send(bytes(cmdToSend.encode("ascii")))  # send the file name
     # time.sleep(3)
 
     filelen = s.recv(10)  # read length of message
     filelen = filelen.decode("ascii")
 
-    print(s.recv(int(filelen)).decode("utf-8"))  # receive info
+    temp = s.recv(int(filelen))
+
+    print(temp.decode("utf-8"))  # receive info
     s.close  # Close the socket when done
